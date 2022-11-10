5d164a4e81d34ecaba261826de6e5bd8c909d577
kulipaxxx <1845472368@qq.com>
2022-11-10 21:06:18 +0800

    add project

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Login from '../views/Login.vue'
+import BlogDetail from '../views/BlogDetails.vue'
+import BlogEdit from '../views/BlogEdit.vue'
 Vue.use(VueRouter)
-
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Index',
+    redirect: { name: 'Blogs' }
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/blogs',
+    name: 'Blogs',
+    // 懒加载
+    component: () => import('../views/Blogs.vue')
+  },
+  {
+    path: '/blog/add', // 注意放在 path: '/blog/:blogId'之前
+    name: 'BlogAdd',
+    meta: {
+      requireAuth: true
+    },
+    component: BlogEdit
+  },
+  {
+    path: '/blog/:blogId',
+    name: 'BlogDetail',
+    component: BlogDetail
+  },
+  {
+    path: '/blog/:blogId/edit',
+    name: 'BlogEdit',
+    meta: {
+      requireAuth: true
+    },
+    component: BlogEdit
   }
-]
-
+];
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
-
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <el-container>
+            <el-header>
+                <img src="" alt="">
+            </el-header>
+            <el-main>
+                <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                    <el-form-item label="用户名" prop="username">
+                        <el-input v-model="ruleForm.username"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                        <el-input type="password" v-model="ruleForm.password"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+                        <el-button @click="resetForm('ruleForm')">重置</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-main>
+        </el-container>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Login",
+
+        data() {
+            return {
+                ruleForm: {
+                    username: 'cblog',
+                    password: '111111',
+                },
+                rules: {
+                    username: [
+                        { required: true, message: '请输入用户名', trigger: 'blur' },
+                        { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+                    ],
+                    password: [
+                        { required: true, message: '请输入密码', trigger: 'change' }
+                    ],
+
+                }
+            };
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        const _this = this;
+                        this.$axios.post('/login',this.ruleForm).then(res=>{
+                            const token = res.headers['authorization']
+                            _this.$store.commit('SET_TOKEN', token)
+                            _this.$store.commit('SET_USERINFO', res.data.data)
+                            _this.$router.push("/blogs")
+                        })
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
+
+    .el-aside {
+        /*background-color: #D3DCE6;*/
+        /*color: #333;*/
+        text-align: center;
+        line-height: 200px;
+    }
+
+    .el-main {
+        /*background-color: #E9EEF3;*/
+        /*color: #333;*/
+        text-align: center;
+        line-height: 160px;
+    }
+
+    body > .el-container {
+        margin-bottom: 40px;
+    }
+
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+        line-height: 260px;
+    }
+
+    .el-container:nth-child(7) .el-aside {
+        line-height: 320px;
+    }
+</style>
